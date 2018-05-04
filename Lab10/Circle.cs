567baab131bb217aae2bed083f98547e91b7027a
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-05-04 08:35:00 -0400

    First commit

---
+++ Lab10/Circle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab10
+{
+    class Circle
+    {
+        const double pi = Math.PI;
+        #region DataMembers
+        //data members
+        private double _radius;
+        #endregion
+
+        #region Properties
+        //properties
+        public double Radius
+        {
+            set //give a value to _radius
+            {
+                _radius = value;
+            }
+            get //give a value to _radius
+            {
+                return _radius;
+            }
+        }
+        #endregion
+
+        #region CalculateCircumference
+        public double CalculateCircumference()
+        {
+            double circumference = 2*(pi * Radius);
+            return circumference;
+        }
+        #endregion
+
+        #region CalculateArea
+        public double CalculateArea()
+        {
+            double area = (Radius * Radius) * pi;
+            return area;
+        }
+        #endregion
+
+        #region CalculateFormattedCircumference
+        public string CalculateFormattedCircumference()
+        {
+            double circumference = 2 * (pi * Radius);
+            return($"{circumference, 0:N2}");
+        }
+        #endregion
+
+        #region CalculateFormattedArea
+        public string CalculateFormattedArea()
+        {
+            double area = (Radius * Radius) * pi;
+            return ($"{area,0:N2}");
+        }
+        #endregion
+
+
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lab10
+{
+    class Program
+    {
+        //set a counter for the program
+        static int studentCounter = 0;
+        static void Main(string[] args)
+        {
+            //set a bool statement for my outside while loop
+            bool resume = true;
+            
+            //instantiate
+            Circle CC = new Circle();
+            while (resume)
+            {
+                //asking user for input
+                Console.Write("Enter a radius: ");
+                //getting user input
+                CC.Radius = Double.Parse(Console.ReadLine());
+                Console.WriteLine($"The area is: {CC.CalculateArea()}");
+                Console.WriteLine($"The circumference is: {CC.CalculateCircumference()}");
+                Console.WriteLine($"The formatted circumference is: {CC.CalculateFormattedCircumference()}");
+                Console.WriteLine($"The formatted area is: {CC.CalculateFormattedArea()}");
+                studentCounter++;
+                while (true)
+                {
+                    Console.WriteLine("(1) to quit or (2) to add another circle");
+                    string input = Console.ReadLine();
+                    if (input == "1")
+                    {
+                        Console.WriteLine($"You added {studentCounter} circles, goodbye!");
+                        resume = false;
+                        break;
+                    }
+                    else if (input == "2")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, try again");
+                    }
+                }
+
+                
+
+            }
+        }
+    }
+}
